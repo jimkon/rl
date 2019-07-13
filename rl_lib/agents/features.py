3330494a38ea6611fb01ccea9ca431c5fd462d77
jimkon <dkontzedakis@gmail.com>
2019-07-13 17:28:18 +0300

    add beta arguement to feature agent

--- rl_lib/agents/features.py
+++ rl_lib/agents/features.py
@@ -5,22 +5,22 @@ import rl_lib as rl
 
 class FeatureAgent(rl.Agent):
 
-    def __init__(self, features):
-        self.features = np.array(features)
-        self.state_dimensions = self.features.shape[1]
-        self.action_dimensions = self.features.shape[0]
-        self.bins = np.arange(self.action_dimensions)
+    def __init__(self, features, actions_num, include_beta=False):
+        self.features = np.array(features if not include_beta else features[:-1])
+        self.state_dims = self.features.shape[0] if not include_beta else self.features.shape[0]-1
+        self.actions_num = actions_num
+        self.bins = np.arange(self.actions_num-1)
+        self.beta = features[-1] if include_beta else .0
 
     def act(self, state):
-        return np.digitize(np.matmul(self.features, state), bins=self.bins).astype(np.int)[0]
+        return np.digitize(np.dot(self.features, state)+self.beta, bins=self.bins).astype(np.int)
 
     def get_features(self):
-        return self.features
+        return self.features, self.beta
 
 
 class RandomFeatureAgent(FeatureAgent):
 
-    def __init__(self, state_dims, actions_dims, distribution=np.random.normal, **distr_args):
-        features = distribution(size=(actions_dims, state_dims), **distr_args)
-        super().__init__(features)
-
+    def __init__(self, state_dims, actions_num, distribution=np.random.normal, **distr_args):
+        features = distribution(size=(state_dims+1), **distr_args)
+        super().__init__(features, actions_num, include_beta=True)
