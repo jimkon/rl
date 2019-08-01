95b3dd5202fcff634d7d0f3e7bceaa39e760921e
jimkon <dkontzedakis@gmail.com>
2019-08-01 14:53:20 +0300

    policy agent

--- rl_lib/agents/policy_gradient.py
+++ rl_lib/agents/policy_gradient.py
@@ -3,10 +3,11 @@ import tensorflow as tf
 
 import rl_lib as rl
 
+
 class PolicyModel(rl.nets.FullyConnectedDNN):
 
     def __init__(self, input_dims, output_dims, gamma=.99, lr=1e-2, **kwargs):
-        super().__init__(input_dims, output_dims, **kwargs, output_activation=None, output_use_bias=False)
+        super().__init__(input_dims, output_dims, **kwargs, output_activation=tf.nn.softmax, output_use_bias=False)
 
         self.gamma = gamma
 
@@ -27,8 +28,6 @@ class PolicyModel(rl.nets.FullyConnectedDNN):
         self.loss = -tf.reduce_sum(self.advantages*tf.log(self.pi_s_a))
 
         self.train = tf.compat.v1.train.AdamOptimizer(lr).minimize(self.loss)
-
-
 
     def policy(self, states, actions=None):
         states = np.atleast_2d(states)
@@ -65,11 +64,39 @@ class PolicyModel(rl.nets.FullyConnectedDNN):
 
         self.sess.run(self.train, feed_dict={self.x: states,
                                              self.actions: actions,
-                                             self.model.gammas: self.GAMMAS[-size:],
-                                             self.model.rewards: rewards,
-                                             self.model.vs: vs
+                                             self.gammas: self.GAMMAS[-size:],
+                                             self.rewards: rewards,
+                                             self.vs: vs
                                              })
 
+    def td_update(self, state, action, reward, v):
+        raise NotImplementedError
+
+
+class ValueModel(rl.nets.FullyConnectedDNN):
+
+    def __init__(self, input_dims, gamma=.99, lr=1e-2, **kwargs):
+        super().__init__(input_dims, output_dims=1, output_activation=None, output_use_bias=False, **kwargs)
+
+    def value(self, states):
+        states = np.atleast_2d(states)
+
+        assert states.shape[1] == self.input_dims
+
+        return self.predict(states).flatten()
+
+    def full_episode_update(self, states, rewards):
+
+        states = np.atleast_2d(states)
+        rewards = np.atleast_2d(rewards)
+
+        size = len(states)
+        assert len(rewards) == size
+
+        self.fit(states, rewards)
+
+    def td_update(self, state, reward):
+        raise NotImplementedError
 
 
 model = PolicyModel(input_dims=2, output_dims=3, drop_out=.0)
@@ -80,27 +107,23 @@ model = PolicyModel(input_dims=2, output_dims=3, drop_out=.0)
 # exit()
 class PGAgent(rl.Agent):
 
-    def __init__(self, state_dims, actions_num, rbf_args=None, nn_args=None, mapper=rl.utils.Mapper):
+    def __init__(self, state_dims, actions_num, actor_args=None, critic_args=None, mapper=rl.utils.Mapper, td_update=False):
         super().__init__(state_dims=state_dims, actions_num=actions_num)
 
-        if rbf_args is None:
-            rbf_args = {'samplers':None,
-                        'constant_samplers':False,
-                        'constant_gammas:':True}
+        assert not td_update, 'Temporal Difference update is not implemented yet'
+        self.td_update = td_update
 
-        if nn_args is None:
-            nn_args = {'hidden_layers':[200, 100],
-                       'activations':[tf.nn.relu, tf.nn.relu],
-                       'use_biases':[True, True],
-                       'output_activation':tf.nn.softmax}
+        if actor_args is None:
+            actor_args = {}
+
+        if critic_args is None:
+            critic_args = {}
 
         self.mapper = mapper
 
-        self.policy_net = rl.nets.FullyConnectedDNN(input_dims=state_dims, output_dims=actions_num, **nn_args)
+        self.actor = PolicyModel(input_dims=state_dims, output_dims=actions_num, **actor_args)
 
-        self.nets = [rl.nets.RBFNet(**rbf_args) for _ in range(self.actions_num)]
-        for net in self.nets:
-            net.create_net(state_dims, 1)
+        self.critic = ValueModel(input_dims=state_dims, **critic_args)
 
         self.states = []
         self.actions = []
@@ -108,29 +131,31 @@ class PGAgent(rl.Agent):
 
         self.episode = 0
 
-    def policy_model(self, input_dims, output_dims, **nn_args):
-        nn = rl.nets.FullyConnectedDNN(input_dims=input_dims, output_dims=output_dims, **nn_args)
-
-
-    def policy(self, state):
-        res = self.policy_net.predict(state)
-        return res
-
-    def full_episode_update_policy(self, states, actions, rewards, gamma=.999):
-        pass
+    def act(self, state):
+        probs = self.policy(state)
+        return np.random.choice(len(probs), 1, p=probs)
 
     def observe(self, state, action, reward, state_, episode=-1, step=-1):
         super().observe(state, action, reward, state_, episode, step)
 
-        if self.episode < episode:
-            self.full_episode_update_policy(self.states, self.actions, self.rewards)
+        if self.td_update:
+            self.actor.td_update(state, action, reward)
+            self.critic.td_update(state, reward)
+        else:
+            if self.episode < episode:
+                self.actor.full_episode_update(self.states, self.actions, self.rewards)
+                self.critic.full_episode_update(self.states, self.rewards)
 
-            self.states = []
-            self.actions = []
-            self.rewards = []
+                self.states = []
+                self.actions = []
+                self.rewards = []
 
-        self.states.append(state)
-        self.actions.append(action)
-        self.rewards.append(reward)
-        self.episode = episode
+            self.states.append(state)
+            self.actions.append(action)
+            self.rewards.append(reward)
+            self.episode = episode
+
+    def policy(self, state):
+        res = self.actor.predict(state)
+        return res
 
